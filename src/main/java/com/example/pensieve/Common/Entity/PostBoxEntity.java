f5fe19e22349c835cb0dbfcbee05994e6a4bfd47
Min kyung suk <smk93021@gmail.com>
2023-09-27 17:08:56 +0900

    entity setting(on process)

--- src/main/java/com/example/pensieve/Common/Entity/PostBoxEntity.java
+++ src/main/java/com/example/pensieve/Common/Entity/PostBoxEntity.java
@@ -1,27 +1,37 @@
 package com.example.pensieve.Common.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 
 public class PostBoxEntity extends BaseEntity{
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false, nullable = false, columnDefinition = "BIGINT UNSIGNED")
     private Long postId;
 
     @OneToOne
-    private UserEntity userId;
+    @JoinColumn(name = "userId", nullable = false)
+    private UserEntity userEntity;
 
-    @NotNull
+    @Column(nullable = false, columnDefinition = "char(1)")
     private int banYn;
+    //삭제여부
 
+    @Column(nullable = false, columnDefinition = "BIGINT UNSIGNED")
+    @ColumnDefault(value = "0")
     private Long hits;
 
+    @Column(nullable = false, columnDefinition = "BIGINT UNSIGNED")
+    @ColumnDefault(value = "0")
     private Long likes;
 
+    @Column(length = 100)
+    @Size(min = 5,max = 100)
     private String ctnt;
 
 
--- src/main/java/com/example/pensieve/Common/Entity/UserEntity.java
+++ src/main/java/com/example/pensieve/Common/Entity/UserEntity.java
@@ -1,13 +1,17 @@
 package com.example.pensieve.Common.Entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Table(name="user")
@@ -15,18 +19,27 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @NoArgsConstructor
 @ToString(callSuper = true)
-
 public class UserEntity extends BaseEntity{
     @Id
     private String userId;
 
+    @Column(nullable = false, length = 50, unique = true)
+    @Size(min = 5, max = 50)
     private String email;
     //unique
 
+    @JsonIgnore
+    @Column(nullable = false)
     private String pw;
+    //size 제약 추가할것
 
+    @Column(nullable = false, length = 10, unique = true)
+    @Size(min = 2, max = 10)
     private String nickNm;
 
+    @Column(nullable = false, length = 1)
+    @ColumnDefault(value = "0")
     private int outYn;
+    //탈퇴여부
 
 }
