b6260f4b9ef141a8ebccd4f5970c05505c34c07e
Min kyung suk <smk93021@gmail.com>
2023-10-04 22:44:57 +0900

    security userdetail config settings

---
+++ src/main/java/com/example/pensieve/common/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,20 @@
+package com.example.pensieve.common.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+@Slf4j
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest req, HttpServletResponse res, AccessDeniedException accessDeniedException)throws IOException, ServletException {
+        log.info("{customAccessDeniedHandler: 접근이 막혔을 경우 리다이렉트}");
+        res.sendRedirect("/api/exception");
+    }
+
+}
---
+++ src/main/java/com/example/pensieve/common/security/MyUserDetails.java
@@ -0,0 +1,45 @@
+package com.example.pensieve.common.security;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+public class MyUserDetails implements UserDetails {
+    private Long userId;
+    private String email;
+    private String pw;
+
+
+    @Override
+    public String getPassword(){
+        return this.pw;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return null;
+        //Todo : 여기가몬데 ?? Collection 그리고 ? extends GrantedAuthority 는 뭔디?
+
+    }
+
+    @Override
+    public String getUsername(){
+
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
+}
+}
