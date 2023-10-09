e2335510f62363cbd941ab47856cc82bfecd1766
Min kyung suk <smk93021@gmail.com>
2023-10-09 15:44:58 +0900

    sign configs

---
+++ src/main/java/com/example/pensieve/sign/SignController.java
@@ -0,0 +1,28 @@
+package com.example.pensieve.sign;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
+@Tag(name = "로그인/회원가입")
+public class SignController {
+    private final SignService SIGNSERVICE;
+
+    /*
+    * 1. 회원가입
+    * 2. access토큰 재발행
+    * 3.로그인(과 동시에 access토큰 생성 /저장)
+    * 4. 로그아웃
+    * 5.이메일 중복확인
+    * 6.비밀번호 찾기
+    * 7.이메일 찾기
+    *
+    * */
+
+}
