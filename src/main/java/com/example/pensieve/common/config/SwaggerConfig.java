650628156e81c7f458abc554449f82da04b5bb7b
Min kyung suk <smk93021@gmail.com>
2023-10-18 22:34:08 +0900

    스웨거3.0설정중

--- src/main/java/com/example/pensieve/common/config/SwaggerConfig.java
+++ src/main/java/com/example/pensieve/common/config/SwaggerConfig.java
@@ -1,46 +1,48 @@
 package com.example.pensieve.common.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
-@EnableSwagger2
 @Configuration
+@OpenAPIDefinition
 public class SwaggerConfig {
 
-    @Bean
-    public Docket api(){
-        return new Docket(DocumentationType.OAS_30)
-                .useDefaultResponseMessages(true)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.ant("/**"))
-                .build();
+        @Bean
+        public OpenAPI api(){
+            Info info = new Info().title("swagg test").version("v3 swagger").description("test");
+            return new OpenAPI().components(new Components()).info(info);
+        }
+
     }
 
-    private ApiInfo apiInfo(){
-        Contact ct = new Contact("m","www.naver.com","smk93021@gmail.com");
 
-        return new ApiInfo(
-                "TEST API",
-                "Some custom description of API.",
-                "0.0.1",
-                "Terms of service",
-                new Contact("MemoStack", "https://memostack.tistory.com", "public.devhong@gmail.com"),
-                "License of API", "API license URL", Collections.emptyList());
-    }
+    //스웨거 fox(2)설정파일.
+//    @Bean
+//    public Docket api(){
+//        return new Docket(DocumentationType.OAS_30)
+//                .useDefaultResponseMessages(true)
+//                .apiInfo(apiInfo())
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.ant("/**"))
+//                .build();
+//    }
+//
+//    private ApiInfo apiInfo(){
+//        Contact ct = new Contact("m","www.naver.com","smk93021@gmail.com");
+//
+//        return new ApiInfo(
+//                "TEST API",
+//                "Some custom description of API.",
+//                "0.0.1",
+//                "Terms of service",
+//                ct, "License of API", "API license URL", Collections.emptyList());
+//    }
 
 //    @Bean
 //    public Docket commonApi() {
@@ -53,5 +55,3 @@ public class SwaggerConfig {
 //                .paths(PathSelectors.ant("/**"))
 //                .build();
 //    }
-}
-
