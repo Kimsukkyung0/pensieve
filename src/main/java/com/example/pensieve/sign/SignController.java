46f4c25025e6b559c58b2c7722d9d855870b6a57
Min kyung suk <smk93021@gmail.com>
2023-11-01 00:15:28 +0900

    500에러 수정중 / AnonymousAuthenticationFilter  stackoverflow 에러. 주석처리 후 403에러로 변경? (forbidden)

--- src/main/java/com/example/pensieve/sign/SignController.java
+++ src/main/java/com/example/pensieve/sign/SignController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/sign")
 public class SignController {
     private final SignService SIGNSERVICE;
 
